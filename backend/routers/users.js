1611132818503172f6915b54b90c5c14bba610c1
llahti <llahti@student.hive.fi>
2020-10-14 16:06:23 +0300

    Add mhjony signup backend

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -1,5 +1,34 @@
 const usersRouter = require('express').Router()
 const db = require('../utils/db')
+const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+
+//const { registerValidation } = require('../utils/validation')
+
+const sendEmail = (email, token) => {
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'testing.matcha',
+			pass: 'matcha1234'
+		}
+	});
+
+	const mailOptions = {
+		from: 'testing.matcha@gmail.com',
+		to: email,
+		subject: 'Sending Email using Node.js',
+		text: `Hello! Please click the following link to verify your email http://localhost:5000/verify?token=${token}`
+	};
+
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Email sent: ' + info.response);
+		}
+	});
+}
 
 usersRouter.get('/', (req, resp) => {
 	db.query('SELECT * FROM users', [], (err, res) => {
@@ -21,30 +50,44 @@ usersRouter.get('/:id', (req, resp) => {
 	})
 })
 
-usersRouter.post('/', (req, resp) => {
-	const { firstName, lastName, username, email, password, gender, orientation } = req.body
-	db.query('INSERT INTO users (first_name, last_name, username, email, password, gender, orientation) \
-				VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
-		[firstName, lastName, username, email, password, gender, orientation], (err, res) => {
+usersRouter.post('/', async (req, resp) => {
+	// LETS VALIDATE THE DATA BEFORE MAKE A USER and it comes from validation.js file
+	//const { error } = registerValidation(req.body);
+	//if (error) return res.status(400).send({ message: error.details[0].message });
+
+	const { firstName, lastName, username, email, token } = req.body;
+
+	// Hash password	
+	const saltRounds = 10
+	const hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
+
+	// Add user
+	db.query('INSERT INTO users (first_name, last_name, username, email, password, token) \
+		VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+		[firstName, lastName, username, email, hashedPassword, token],
+		(err, res) => {
 			if (err)
-				resp.status(500).send(err)
+				resp.status(500).send({ error: err.detail })
 			else
 				resp.status(200).send(res.rows[0])
 		})
+
 })
 
 usersRouter.put('/:id', (req, resp) => {
 	const { firstName, lastName, username, email, gender, orientation, tags, bio } = req.body
 	db.query('UPDATE users \
-				SET (first_name, last_name, username, email, gender, orientation, tags, bio) = ($1, $2, $3, $4, $5, $6, $7, $8) \
-				WHERE user_id = $9 RETURNING *',
-		[firstName, lastName, username, email, gender, orientation, tags, bio, req.params.id], (err, res) => {
-			
+		SET (first_name, last_name, username, email, gender, orientation, tags, bio) \
+		= ($1, $2, $3, $4, $5, $6, $7, $8) \
+		WHERE user_id = $9 RETURNING *',
+		[firstName, lastName, username, email, gender, orientation, tags, bio, req.params.id],
+		(err, res) => {
+
 			if (res && res.rows[0])
 				resp.status(200).send(res.rows[0])
 			else if (res)
 				resp.status(500).send({ error: 'User not found' })
-			else 
+			else
 				resp.status(500).send({ error: err.detail })
 		})
 })
