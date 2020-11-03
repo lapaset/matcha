10caa42498221163736fb4df5a235e322b8de729
llahti <llahti@student.hive.fi>
2020-11-03 20:03:44 +0200

    Create user cards

--- frontend/src/components/UserView/index.js
+++ frontend/src/components/UserView/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom'
 import UserProfile from '../UserProfile'
 import Signup from '../Signup'
@@ -6,90 +6,67 @@ import Login from '../Login'
 import Verify from '../Verify'
 import Forgot from '../ForgotPassword'
 import Reset from '../ForgotPassword/resetNewPasswd'
-import userService from '../../services/userService'
+import UserCard from '../UserCard'
 import logoutService from '../../services/logoutService'
 
-
-const UserList = ({ users }) => {
-	return users
-		? <div>
-			<h2>users</h2>
-			<ul>
-				{users.map(u => {
-					return <li key={u.user_id}>{u.user_id}: {u.username}, {u.email}, {u.gender} looking for {u.orientation} </li>
-				})}
-			</ul>
-		</div>
-		: null
-}
-
 const UserView = ({ user, setUser }) => {
-	const [users, setUsers] = useState(false)
-
-	useEffect(() => {
-		userService
-			.getAll()
-			.then(u => {
-				setUsers(u)
-			})
-			.catch((error) => {
-				console.log(error)
-			})
-	}, [])
 
 	const userInfoComplete = () => {
 		return user.firstName && user.lastName && user.username && user.email && user.gender && user.orientation
 	}
-	
+
 	console.log(user, userInfoComplete())
 
 
 	return <Router>
-			<div className="nav">
+		<div className="nav">
+			<div className="navLeft">
 				<Link to="/">home</Link>
+			</div>
+			<div className="navRight">
 				{user.username
-					? <><Link to="/profile">profile</Link>
-						<div>user: {user.username}</div>
-						<Link to="/login" onClick={logoutService.handleLogout}>Logout</Link></>
+					? <><Link to="/profile">{user.username}</Link>
+						<Link to="/login" onClick={logoutService.handleLogout}>logout</Link></>
 
 					: <><Link to="/signup">signup</Link>
 						<Link to="/login">login</Link></>}
 			</div>
-			<Switch>
-				<Route path="/profile" render={() =>
-					user.user_id
-						? userInfoComplete()
-							? <UserProfile user={user} setUser={setUser} />
-							: <><p className="text-center text-info">fill your info to start matching</p>
+		</div>
+		<Switch>
+			<Route path="/profile" render={() =>
+				user.user_id
+					? userInfoComplete()
+						? <UserProfile user={user} setUser={setUser} />
+						: <><p className="text-center text-info">fill your info to start matching</p>
 							<UserProfile user={user} setUser={setUser} /></>
-							
-						: <Redirect to="/login" />
-				} />
-				<Route path="/signup">
-					<Signup />
-				</Route>
-				<Route path="/forgot">
-					<Forgot />
-				</Route>
-				<Route path="/reset-password/:token">
-					<Reset />
-				</Route>
-				<Route path="/login" render={() =>
-					user.user_id ? <Redirect to="/" /> : <Login setUser={setUser} />
-				} />
-				<Route path="/verify">
-					<Verify setUser={setUser} />
-				</Route>
-				<Route path="/" render={() =>
-					user.user_id
-						? userInfoComplete() ? <UserList users={users} /> : <Redirect to="/profile" />
-						: <><h1>Welcome</h1></>
-				} />
-			</Switch>
-			<footer>
-				this is footer
+
+					: <Redirect to="/login" />
+			} />
+			<Route path="/signup">
+				<Signup />
+			</Route>
+			<Route path="/forgot">
+				<Forgot />
+			</Route>
+			<Route path="/reset-password/:token">
+				<Reset />
+			</Route>
+			<Route path="/login" render={() =>
+				user.user_id ? <Redirect to="/" /> : <Login setUser={setUser} />
+			} />
+			<Route path="/verify">
+				<Verify setUser={setUser} />
+			</Route>
+			<Route path="/" render={() =>
+				user.user_id
+					? userInfoComplete() ? <UserCard user={user} /> : <Redirect to="/profile" />
+					: <><h1>Welcome</h1></>
+			} />
+		</Switch>
+		<footer>
+			this is footer
 			</footer>
-		</Router>
+	</Router>
 }
 
 export default UserView
