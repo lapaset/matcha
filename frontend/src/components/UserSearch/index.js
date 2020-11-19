59100cf0fc02b8816af7679cd0ba250eb7120492
llahti <llahti@student.hive.fi>
2020-11-19 09:31:20 +0200

    Refactor

--- frontend/src/components/UserSearch/index.js
+++ frontend/src/components/UserSearch/index.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react'
-import { Form, Col } from 'react-bootstrap'
 import { useFilter } from '../../hooks/index'
 import ListOfUsers from './ListOfUsers'
 import SortForm from './SortForm'
+import FilterForm from './FilterForm'
 
 // fix adding tags
 // fix upload photo preview layout
 
 const UserSearch = ({ user }) => {
 
-	//const [results, setResults] = useState([])
 	const [resultsToShow, setResultsToShow] = useState([])
-	const [hideFilterForm, setHideFilterForm] = useState(true)
 
 	const maxDistance = useFilter('matchaMaxDistance', 100, 'number')
 	const minAge = useFilter('matchaMinAge', 20, 'number')
@@ -20,36 +18,7 @@ const UserSearch = ({ user }) => {
 	const requiredTag = useFilter('matchaRequiredTag', '', 'text')
 
 	const sortFormProps = ({ user, resultsToShow, setResultsToShow })
-	/*
-	const [maxDistance, setMaxDistance] = useState(filterFromStorage('matchaMaxDistance', 100))
-	const [minAge, setMinAge] = useState(filterFromStorage('matchaMinAge', 20))
-	const [maxAge, setMaxAge] = useState(filterFromStorage('matchaMaxAge', 120))
-	const [minFame, setMinFame] = useState(filterFromStorage('matchaMinFame', 100))
-	const [requiredTag, setRequiredTag] = useState(filterFromStorage('matchaRequiredTag', null))
-	const [hideFilterForm, setHideFilterForm] = useState(true)
-
-	const sortFormProps = ({ user, resultsToShow, setResultsToShow })*/
-
-
-
-	/*const handleIntValue = (val, localStorageName, setter) => {
-		const value = parseInt(val)
-
-		if (!value || value < 0)
-			return
-
-		window.localStorage.setItem(localStorageName, value)
-		setter(value)
-	}
-
-	const handleMaxDistance = e => handleIntValue(e.target.value, 'matchaMaxDistance', setMaxDistance)
-	const handleMinAge = e => handleIntValue(e.target.value, 'matchaMinAge', setMinAge)
-	const handleMaxAge = e => handleIntValue(e.target.value, 'matchaMaxAge', setMaxAge)
-	const handleMinFame = e => handleIntValue(e.target.value, 'matchaMinFame', setMinFame)
-	const handleFilterTag = e => {
-		window.localStorage.setItem('matchaRequiredTag', e.target.value)
-		setRequiredTag(e.target.value)
-	}*/
+	const filterFormProps = ({ user, requiredTag, maxDistance, minFame, minAge, maxAge })
 
 	const requiredTagFound = tags => tags && requiredTag.value
 		? tags.split('#').includes(requiredTag.value)
@@ -62,69 +31,11 @@ const UserSearch = ({ user }) => {
 				&& r.fame >= minFame.value && requiredTagFound(r.tags))
 		: []
 
-	//console.log('sortBy', sortBy.current)
-	//console.log('results', results);
-
-
-	console.log('requiredTag', requiredTag)
-	console.log('maxDistance', maxDistance)
-
 	return <>
 
 		<SortForm {...sortFormProps} />
 
-		<Form hidden={hideFilterForm}>
-
-			{user.tags
-				? <Form.Group>
-					<Form.Label>Tag</Form.Label>
-					<Form.Control as="select" {...requiredTag} >
-						{user.tags
-							.split('#')
-							.map(t => <option key={t} value={t}>{t}</option>)}
-					</Form.Control>
-				</Form.Group>
-				: null
-			}
-
-			<Form.Row>
-				<Col>
-					<Form.Group>
-						<Form.Label>Max distance</Form.Label>
-						<Form.Control {...maxDistance} />
-					</Form.Group>
-				</Col>
-				<Col>
-					<Form.Group>
-						<Form.Label>Min fame</Form.Label>
-						<Form.Control {...minFame} />
-					</Form.Group>
-				</Col>
-			</Form.Row>
-
-			<Form.Row>
-				<Col>
-					<Form.Group>
-						<Form.Label>Min age</Form.Label>
-						<Form.Control {...minAge} />
-					</Form.Group>
-				</Col>
-				<Col>
-					<Form.Group>
-						<Form.Label>Max age</Form.Label>
-						<Form.Control {...maxAge} />
-					</Form.Group>
-				</Col>
-			</Form.Row>
-		</Form>
-
-		<div className="text-info text-right mb-3" onClick={() => setHideFilterForm(!hideFilterForm)}>
-			{
-				hideFilterForm
-					? <>show filters</>
-					: <>hide filters</>
-			}
-		</div>
+		<FilterForm {...filterFormProps} />
 
 		<ListOfUsers users={filterResults()} />
 
