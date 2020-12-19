7e979ee41bed2928490f61575efa51ee8b7d9302
llahti <llahti@student.hive.fi>
2020-12-19 14:00:50 +0200

    Create notifications api

--- backend/routes/block.js
+++ backend/routes/block.js
@@ -2,55 +2,50 @@ const blockRouter = require('express').Router()
 const db = require('../utils/db')
 
 blockRouter.post('/', (req, resp) => {
-    db.query('INSERT INTO blocked (from_user_id, to_user_id) VALUES ($1, $2) RETURNING *',
-    [req.body.from_user_id, req.body.to_user_id], (err, res) => {
-        if (res)
-            resp.status(200).send({message: "From now you can't see anything from "});
-        else
-            resp.status(500).send(err);
-    })
+	db.query('INSERT INTO blocked (from_user_id, to_user_id) VALUES ($1, $2) RETURNING *',
+		[req.body.from_user_id, req.body.to_user_id], (err, res) => {
+			if (res)
+				resp.status(200).send({ message: "From now you can't see anything from " });
+			else
+				resp.status(500).send(err);
+		})
 })
 
 blockRouter.post('/no-access', (req, resp) => {
-	db.query('SELECT * FROM blocked WHERE from_user_id = $1 AND to_user_id = $2',
-	[req.body.from_user_id, req.body.to_user_id], (err, res) => {
-		if (res && res.rows[0])
-		{
-			resp.status(200).send({ value: 1 })
+	db.query('SELECT * FROM blocked\
+	WHERE (from_user_id = $1 AND to_user_id = $2) OR (from_user_id = $2 AND to_user_id = $1)',
+		[req.body.from_user_id, req.body.to_user_id], (err, res) => {
+			if (res && res.rows[0]) {
+				resp.status(200).send({ value: 1 })
+			}
+			else if (res)
+				resp.status(200).send({ value: 0 })
+			else
+				resp.status(400).send({ value: "Query execution failed" });
 		}
-		else if (res)
-			resp.status(200).send({ value: 0 })
-		else
-			resp.status(400).send({ value: "Query execution failed"});
-	}
 	)
 })
 
-blockRouter.post('/blocklist', (req, resp) => {
-    //console.log(req.body);
-	db.query('SELECT * FROM users WHERE users.user_id IN (SELECT to_user_id FROM blocked WHERE from_user_id = $1)',
-	[req.body.from_user_id], (err, res) => {
-		if (res && res.rows[0])
-		{
-            //console.log(res.rows)
-            resp.status(200).send(res.rows)
-		}
-		else if (res)
-			resp.status(200).send({ value: 0 })
-		else
-			resp.status(400).send({ value: "Query execution failed"});
-	}
-	)
+blockRouter.get('/:id', (req, resp) => {
+	db.query('SELECT username, block_id, from_user_id, to_user_id FROM blocked\
+	INNER JOIN users ON to_user_id = users.user_id\
+	WHERE to_user_id = $1 OR from_user_id = $1',
+		[req.params.id], (err, res) => {
+			if (res)
+				resp.status(200).send(res.rows)
+			else
+				resp.status(500).send({ error: err.detail })
+		})
 })
 
-blockRouter.post('/unblock', (req, resp) => {
-    db.query('DELETE FROM blocked WHERE from_user_id = $1 AND to_user_id = $2',
-    [req.body.from_user_id, req.body.to_user_id], (err, res) => {
-        if (res)
-            resp.status(200).send({message: "You just unblocked this users"});
-        else
-            resp.status(500).send(err);
-    })
+blockRouter.delete('/:id', (req, resp) => {
+	db.query('DELETE FROM blocked WHERE block_id = $1',
+		[req.params.id], (err, res) => {
+			if (res)
+				resp.status(200).send({ message: "You just unblocked this users" });
+			else
+				resp.status(500).send(err);
+		})
 })
 
 module.exports = blockRouter;
---
+++ backend/routes/notifications.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+const db = require('../utils/db')
+
+router.get('/', (req, resp) => {
+
+	if (req.query.user_id)
+		db.query('SELECT * FROM notifications WHERE user_id=$1 ORDER BY date ASC',
+			[req.query.user_id], (err, res) => {
+				if (res)
+					resp.status(200).send(res.rows)
+				else
+					resp.status(500).send({ error: err.detail })
+			})
+
+	else
+		db.query('SELECT * FROM notifications ORDER BY date ASC', [], (err, res) => {
+			if (res)
+				resp.status(200).send(res.rows)
+			else
+				resp.status(500).send({ error: err.detail })
+		})
+})
+
+router.post('/', (req, resp) => {
+	db.query('INSERT INTO notifications (user_id, notification) \
+	VALUES ($1, $2) RETURNING *', [req.body.user_id, req.body.notification],
+		(err, res) => {
+			if (res)
+				resp.status(201).send(res.rows[0])
+			else
+				resp.status(500).send({ error: err.detail })
+		})
+})
+
+router.patch('/:id', (req, resp) => {
+	db.query('UPDATE notifications SET read = 1 WHERE id = $1',
+		[req.params.id], (err, res) => {
+			if (res)
+				resp.status(204).end()
+			else
+				resp.status(500).send({ error: err.detail })
+		})
+})
+
+module.exports = router
