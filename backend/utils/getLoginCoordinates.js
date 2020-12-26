fb20af87ab04a255890945246b458ccec8513cf0
llahti <llahti@student.hive.fi>
2020-12-26 18:21:54 +0200

    Remove excess logs

--- backend/utils/getLoginCoordinates.js
+++ backend/utils/getLoginCoordinates.js
@@ -1,22 +1,15 @@
-const express = require('express');
 const geoIP = require('geoip-lite');
 const axios = require('axios');
-
 
 const validateCoordinate = coordinate => {
 	const coordFloat = coordinate;
 	return !isNaN(coordFloat) && coordFloat >= -180 && coordFloat <= 180;
-};
-
+}
 
 const getLoginCoordinates = async (req, userData) => {
 	// Check if valid coordinates are provided through browser geolocation and just return them
 	if (validateCoordinate(req.body.latitude) && validateCoordinate(req.body.longitude))
 		return {'latitude': req.body.latitude, 'longitude': req.body.longitude};
-
-	//194.136.126.51
-	//console.log("This is not happening again :(")
-	//console.log(req.ip)
 
 	const lookup = geoIP.lookup(req.ip);
 
@@ -27,7 +20,6 @@ const getLoginCoordinates = async (req, userData) => {
 		var location = await axios.get('https://ipinfo.io/geo')
 		var locs = location.data.loc.split(',');
 
-		console.log('locs', locs)
 		return { latitude: locs[0], longitude: locs[1] }
 	}
 
