32a7fa2e364bfb577134153b2c4d6088d37fcc5b
llahti <llahti@student.hive.fi>
2020-12-27 11:54:11 +0200

    Separate app from server index

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,8 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
 const config = require('./utils/config')
-const middleware = require('./utils/middleware')
-const routes = require('./routes')
-
-app.use(express.json({ limit: '10mb', extended: true }))
-app.use(express.urlencoded({ limit: '10mb', extended: true }))
-app.use(cors())
-app.use(middleware.requestLogger)
-app.use(middleware.tokenExtractor)
-app.enable('trust proxy')
-
-Object.keys(routes).forEach(k => {
-	app.use(`/${k}`, routes[k])
-})
-
-app.use(middleware.unknownEndpoint)
+const app = require('./app')
 
 const server = app.listen(config.PORT, () => {
 	console.log(`App running on port ${config.PORT}.`)
 })
 
-const wsServer = require('./socket')(server)
-
+require('./socket')(server)
